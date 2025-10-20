3d05853555026f1ea980eabf83d308fb60b2d1f2
William De Sa <willrdesa@gmail.com>
2025-10-20 17:34:49 -0400

    Moves and captures

--- main.c
+++ main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include "game.h"
 
-//Declarations so the compiler doesn't hate me
-unsigned long long* setBoard(void);
-void printBoard(unsigned long long redBoard, unsigned long long blackBoard);
-void checkLegalMoves(unsigned long long redBoard, unsigned long long blackBoard, int player, int legalMoves[][2]);
-
 int main() {
     printf("# Notation: x,y\n");
     printf("# x: current position of piece\n");
@@ -14,35 +9,41 @@ int main() {
     for (int i = 0; i < 8; i++) {
         printf("# ");
         for (int j = i * 8; j < 8 * (i + 1); j++) {
+            if (j < 10) printf(" ");
             printf("%d ", j);
         }
         printf("\n");
     }
     printf("\n################################\n\n");
 
-
-
-    unsigned long long redBoard;
-    unsigned long long blackBoard;
-
     unsigned long long *boards = setBoard();
-    blackBoard = boards[0];
-    redBoard = boards[1];
 
     int stop = 0;
     int isRed = 1;
 
     while (!stop) {
-        printBoard(redBoard, blackBoard);
+        printBoard(boards);
+        if (isRed == 1) printf("RED TURN:\n");
+        else printf("BLACK TURN:\n");
 
-        int legalMoves[12][2];
-        checkLegalMoves(redBoard, blackBoard, isRed, legalMoves);
-        if (isRed == 1) printf("Red turn: ");
-        else printf("Black turn: ");
-        int move;
-        scanf("%d", &move);
+        int x, y;
+        printf("x: ");
+        scanf("%d", &x);
+        printf("y: ");
+        scanf("%d", &y);
         printf("\n");
+        move(boards, x, y, isRed);
+        capture(boards, x, y, isRed);
+
+        if (checkWin(boards) == 0) {
+            printf("BLACK WINS!");
+            break;
+        }
+        else if (checkWin(boards) == 1) {
+            printf("RED WINS!");
+            break;
+        }
+
         isRed = !isRed;
-        if (move == -1) break;
     }
 }
